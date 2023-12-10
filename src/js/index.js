3e41753906706622c5c806b70c120c84c1c22f2b
Irina Khorunz <irahorunz87@gmail.com>
2023-12-10 16:23:19 +0200

    adds render main menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,35 @@
-// import mobileNav from "./modules/mobile-nav.js";
-// mobileNav();
-
 import data from "../data/data.json";
 
 const list = document.querySelector(".list-menu");
 
-const markup = data.reduce((acc, { name }) => {
-  return (
+const markup = data.reduce(
+  (acc, { name }) =>
     acc +
     `<li class="list-menu__item">
-       ${name}
-     </li>`
-  );
-});
+       <p class="list-menu__item-title">${name}</p>
+       <div class="selection-box">
+       <p class="selection"></p>
+       <p class="selection"></p>
+       <p class="selection"></p>
+       <p class="selection"></p>
+       <p class="selection"></p>
+       </div>
+     </li>`,
+  ""
+);
 
 list.insertAdjacentHTML("beforeend", markup);
+
+list.addEventListener("click", onClick);
+
+function onClick(e) {
+  if (e.target.classList.contains("selection")) {
+    e.target.classList.toggle("selection--active");
+    if (e.target.classList.contains("selection--active")) {
+      e.target.textContent = "1";
+    } else {
+      e.target.textContent = "";
+    }
+  }
+  return;
+}
